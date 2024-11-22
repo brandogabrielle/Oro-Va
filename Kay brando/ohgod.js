501741c6565aedc4318c551a01142162126be11e
brandogabrielle <68659024+brandogabrielle@users.noreply.github.com>
2024-11-22 15:54:12 +0800

    Add files via upload

---
+++ Kay brando/ohgod.js
@@ -0,0 +1,16 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll' , () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+    header.classList.remove('full', window.scrollY > 0);
+});
